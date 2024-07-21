b2a46167e5a5c4ba658c359afc251290770662c8
ngviet00 <ngvviet0306@gmail.com>
2024-07-21 21:32:15 +0700

    remove folder backup

--- CIM/CIM/Program.cs
+++ CIM/CIM/Program.cs
@@ -1,30 +1,22 @@
 ﻿using System;
-using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CIM
 {
     internal static class Program
     {
-        static readonly Mutex mutex = new Mutex(true, "{b74f4e7a-7326-4b84-8d33-2f2d56bdf1f3cimpc}");
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            if (mutex.WaitOne(TimeSpan.Zero, true))
-            {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
-                mutex.ReleaseMutex();
-            }
-            else
-            {
-                MessageBox.Show("The program is running!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
